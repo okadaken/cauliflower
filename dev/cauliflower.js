c4691edafa58f8eb915e90afe78782368ef12ceb
manab <1431162+manab@users.noreply.github.com>
2012-08-15 10:51:51 +0900

    HTMLとJavaScriptの合成、HTMLパースのエラーハンドリングを実装した。

---
+++ dev/cauliflower.js
@@ -0,0 +1,146 @@
+/**
+ * Cauliflower v0.1
+ *
+ * Copyright 2012 okadaken & gakuchan
+ * http://github.com/okadaken/cauliflower
+ */
+var HTMLEditor;
+
+$(document).ready(function() {
+    initializeTabs();
+    initializeButtons();
+    initializeHTMLEditor();
+    initializeDialogs();
+    
+    setTimeout(updatePreview, 300);
+});
+
+function initializeTabs() {
+    $("#tabs").tabs({
+        cookie: {
+            expires: 3 //選択タブをcookieで3日間保存
+        },
+        select: function(event, ui) {
+            if (ui.index == 0) {
+                Blockly.mainWorkspace.render();
+            } else 
+                if (ui.index == 1) {
+                
+                }
+        }
+    });
+}
+
+function initializeButtons() {
+    $("#new_button").button().click(function() {
+        alert("新規作成");
+    });
+    $("#open_button").button().click(function() {
+        alert("読み込み");
+    });
+    $("#save_button").button().click(function() {
+        alert("保存する");
+    });
+    $("#exec_button").button().css({
+        "background": "#ffcc00"
+    }).click(function() {//TODO:とりあえず色を変えただけ 要グラデーション
+        alert("実行する");
+    });
+    $("#html_new_botton").button();
+}
+
+function initializeHTMLEditor() {
+    var delay;
+    HTMLEditor = CodeMirror.fromTextArea(document.getElementById("html_textarea"), {
+        mode: "text/html",
+        theme: "default",
+        tabMode: "indent",
+        lineNumbers: "true",
+        fixedGutter: "true",
+        electricChars: "true",
+        onChange: function() {
+            clearTimeout(delay);
+            delay = setTimeout(updatePreview, 300);
+        }
+    });
+    var httpObj = $.get("html_template.txt", function() {
+        HTMLEditor.setValue(httpObj.responseText);
+        HTMLEditor.save();
+    });
+}
+
+function initializeDialogs() {
+    $("#HTML_syntaxerror_dialog").dialog({
+        dialogClass: "alert",
+        autoOpen: false,
+        modal: true,
+        draggable: false,
+        resizable: false,
+        show: "clip",//"explode",
+        hide: "clip",//"explode",
+        title: "HTML構文エラー",
+        buttons: {
+            OK: function() {
+                $(this).dialog("close");
+            }
+        }
+    });
+}
+
+function getHTMLCode() {
+    return HTMLEditor.getValue();
+}
+
+//Blocklyのエディタができるまでの仮実装
+function getJavaScriptCode() {
+    return "\nalert(\"Hello World!\");\n";
+}
+
+function getAllCode() {
+    //前処理としてHTMLをXMLに変換
+    try {
+        var xml = HTMLtoXML(getHTMLCode());
+    } catch (e) {//変換できなければエラー表示して終了
+        var errorLine = e.split("\n")[0];
+        var message = "<p>HTMLの構文エラーです。</p><span style=\"background: #ffaaaa;\">" + htmlEscape(errorLine) + "</span><p>上記のHTMLを正しく修正してください。</p>";
+        $("#HTML_syntaxerror_dialog_message").html(message);
+        $("#HTML_syntaxerror_dialog").dialog("open");
+        return;
+    }
+    
+    //DOMにパース
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(xml, "text/xml");
+    
+    //JavaScriptタグとコードのノードを生成
+    var js = doc.createElement("script");
+    js.setAttribute("type", "text/javascript");
+    js.appendChild(doc.createTextNode(getJavaScriptCode()));
+    
+    //生成したノードを追加
+    var body = doc.getElementsByTagName("body")[0];
+    if (body != null) {
+        body.insertBefore(js, body.firstChild);
+        body.insertBefore(doc.createTextNode("\n"), body.firstChild);//JavaScriptタブの前に改行を追加
+        var serializer = new XMLSerializer();
+        var code = serializer.serializeToString(doc);
+        console.log(code); //TODO:最後に行頭のDOCTYPEを追加すること
+    } else {
+        alert("bodyタグが見つかりません");//TODO:dialogにすること
+    }
+}
+
+function updatePreview() {
+    var previewFrame = document.getElementById("html_preview");
+    var preview = previewFrame.contentDocument || previewFrame.contentWindow.document;
+    preview.open();
+    preview.write(HTMLEditor.getValue());
+    preview.close();
+}
+
+function htmlEscape(s) {
+    s = s.replace(/&/g, '&amp;');
+    s = s.replace(/>/g, '&gt;');
+    s = s.replace(/</g, '&lt;');
+    return s;
+}
---
+++ dev/tmp.js
@@ -0,0 +1,74 @@
+
+    
+                                   var editor;
+                                    $(function(){
+                                    	var delay;
+                                        editor = CodeMirror.fromTextArea(document.getElementById("html_textarea"), {
+                                          mode: "text/html",
+                                          theme: "default",
+                                          tabMode: "indent",
+                                          lineNumbers: "true",
+                                          fixedGutter: "true",
+                                          electricChars: "true",
+                                          onChange: function() {
+                                            clearTimeout(delay);
+                                            delay = setTimeout(updatePreview, 300);
+                                          }
+                                          
+                                          
+                                        });
+                                        
+                        
+                                        
+                                   
+                                        
+                                        
+                                        function updatePreview() {
+                                          var previewFrame = document.getElementById("html_preview");
+                                          var preview =  previewFrame.contentDocument ||  previewFrame.contentWindow.document;
+                                          preview.open();
+                                          preview.write(editor.getValue());
+                                          preview.close();
+                                        }
+                                        setTimeout(updatePreview, 300);
+                                    });
+                                    
+
+                                    
+                                    /**
+                                     * Initialize Blockly.  Called on page load.
+                                     * @param {!Blockly} blockly Instance of Blockly from iframe.
+                                     */
+                                    function init(blockly) {
+                                      //window.onbeforeunload = function() {
+                                      //  return 'Leaving this page will result in the loss of your work.';
+                                      //};
+                                    
+                                      window.Blockly = blockly;
+                                    
+                                      // Make the 'Blocks' tab line up with the toolbox.
+                                      if (Blockly.Toolbox) {
+                                        Blockly.bindEvent_(window, 'resize', null, function() {
+                                          document.getElementById('tab_blocks').style.minWidth =
+                                              (Blockly.Toolbox.width - 38) + 'px';
+                                              // Account for the 19 pixel margin and on each side.
+                                          });
+                                        window.setTimeout(function() {
+                                            Blockly.fireUiEvent(document, window, 'resize');
+                                          }, 1);
+                                      }
+                                    
+                                      // Restore/backup current works.
+                                      restore_blocks();
+                                      restore_htmls();
+                                      Blockly.bindEvent_(window, 'unload', null, backup_blocks);
+                                      Blockly.bindEvent_(window, 'unload', null, backup_htmls);
+                                      tabClick('tab_' + selected);
+                                    
+                                      // Init load event.
+                                      var loadInput = document.getElementById('load');
+                                      loadInput.addEventListener('change', load, false);
+                                      document.getElementById('fakeload').onclick = function() {
+                                        loadInput.click();
+                                      };
+                                    }
