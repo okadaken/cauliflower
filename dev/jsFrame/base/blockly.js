812c1a7ec210bcb85cc103677fb64ddb818e0374
manab <1431162+manab@users.noreply.github.com>
2012-08-18 04:27:55 +0900

    BackSpaceKeyで削除時にコード生成。

--- dev/jsFrame/base/blockly.js
+++ dev/jsFrame/base/blockly.js
@@ -315,6 +315,7 @@ Blockly.onKeyDown_ = function(e) {
       Blockly.hideChaff();
       Blockly.playAudio('delete');
       Blockly.selected.destroy(true);
+      window.parent.updateJavaScriptPreview(Blockly.Generator.workspaceToCode('JavaScript'));
     }
     // Stop the browser from going back to the previous page.
     e.preventDefault();
