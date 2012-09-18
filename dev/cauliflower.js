c5fcaaa66b11fef3b005becb5fbf04b91d54b593
manab <1431162+manab@users.noreply.github.com>
2012-09-19 01:17:54 +0900

    選択範囲が無かった時の処理を追加。

--- dev/cauliflower.js
+++ dev/cauliflower.js
@@ -1056,6 +1056,10 @@ function commentSelection(isComment) {
     from: HTMLEditor.getCursor(true),
     to: HTMLEditor.getCursor(false)
   };
+  
+  if (rang.from.line == rang.to.line && rang.from.ch == rang.to.ch) {
+    return;
+  }
   HTMLEditor.commentRange(isComment, range.from, range.to);
   HTMLEditor.setCursor(cursor);
   HTMLEditor.focus();
